76545be763918a5b410aa912f10a039cb85dd38c
payencai <payencai@sina.com>
2019-04-16 19:40:57 +0800

    完成代驾

--- app/src/main/java/com/system/X5WebviewActivity.java
+++ app/src/main/java/com/system/X5WebviewActivity.java
@@ -2,6 +2,7 @@ package com.system;
 
 import android.content.Intent;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -31,7 +32,7 @@ public class X5WebviewActivity extends AppCompatActivity {
 
     X5WebView mX5WebView;
    private void getCity(double latitude,double longitude){
-       String url="https://restapi.amap.com/v3/geocode/regeo?output=json&location="+longitude+","+latitude+"&key=b3e064ba536662c892cd0c81169b5bfb&extensions=all&batch=false";
+       String url="https://restapi.amap.com/v3/geocode/regeo?output=json&location="+longitude+","+latitude+"&key=bbe8f89a8956d407bc8dec2b26a18ab9&extensions=all&batch=false";
        //String url="https://restapi.amap.com/v3/geocode/regeo?output=json&location=113.39679,23.04551&key=b3e064ba536662c892cd0c81169b5bfb&extensions=all&batch=false";
        HttpProxy.obtain().get(url, "", new ICallBack() {
            @Override
@@ -98,6 +99,11 @@ public class X5WebviewActivity extends AppCompatActivity {
                 }
             }
 
+            @Override
+            public void onPageStarted(WebView webView, String s, Bitmap bitmap) {
+                super.onPageStarted(webView, s, bitmap);
+
+            }
 
             @Override
             public void onPageFinished(WebView view, String url) {
@@ -108,8 +114,8 @@ public class X5WebviewActivity extends AppCompatActivity {
         mX5WebView.setWebChromeClient(new WebChromeClient() {
 
         });
-        //mX5WebView.loadUrl("http://120.79.176.228:8080/gaote-web/map/index.html");
-        mX5WebView.loadUrl("https://apis.map.qq.com/tools/locpicker?search=1&type=0&backurl=http://3gimg.qq.com/lightmap/components/locationPicker2/back.html&key=OB4BZ-D4W3U-B7VVO-4PJWW-6TKDJ-WPB77&referer=myapp");
+        mX5WebView.loadUrl("http://120.79.176.228:8080/gaote-web/map/index.html");
+        //mX5WebView.loadUrl("https://apis.map.qq.com/tools/locpicker?search=1&type=0&backurl=http://3gimg.qq.com/lightmap/components/locationPicker2/back.html&key=OB4BZ-D4W3U-B7VVO-4PJWW-6TKDJ-WPB77&referer=myapp");
 
     }
 }
