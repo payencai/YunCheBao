8361abe2b89911cd5b5e87ab630d5e3602d8926a
mr <18182914406@163.com>
2019-04-25 08:58:35 +0800

    init 自驾订单

---
+++ app/src/main/java/com/cheyibao/SelfDriverOrderActivity.java
@@ -0,0 +1,15 @@
+package com.cheyibao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yunchebao.R;
+
+public class SelfDriverOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_self_driver_order);
+    }
+}
